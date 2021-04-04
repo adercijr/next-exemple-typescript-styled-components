8a4b181af8e540949353faea77f54446ffc4a1aa
Aderci Jr <adercidepaula.jr@gmail.com>
2021-04-04 02:26:06 +0100

    first commit

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: red
+  }
+`;
